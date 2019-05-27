419ff47e87c54fc1f691c34480fba82fe8834f3b
ibronos <ibronossaidi@gmail.com>
2019-05-27 00:33:28 -0400

    123

---
+++ resources/views/layout/partials/footer-scripts.blade.php
@@ -0,0 +1,3 @@
+<!-- <script src="{{ asset('js/jquery.min.js') }}"></script> -->
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script> -->
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,42 @@
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="row">
+      <div class="col-sm-4" style="background-color:yellow; padding: 10px 0px 10px 0px;">
+          <div class="row justify-content-center">
+              <i class="fa fa-facebook fa-2x fa-fw"></i>
+              <h5>Jual</h5>
+          </div>
+      </div>
+      <div class="col-sm-4" style="background-color:orange; padding: 10px 0px 10px 0px;">
+          <div class="row justify-content-center">
+              <i class="fa fa-facebook fa-2x fa-fw"></i>
+              <h5>Cari</h5>
+          </div>
+      </div>
+      <div class="col-sm-4" style="background-color:yellow; padding: 10px 0px 10px 0px;">
+          <div class="row justify-content-center">
+              <i class="fa fa-facebook fa-2x fa-fw"></i>
+              <h5>Donasi</h5>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div>
+        <a class="navbar-brand" href="#">
+          <!-- <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""> -->
+          <i class="fa fa-facebook fa-2x fa-fw"></i>
+        Bootstrap
+        </a>
+      </div>
+    <div>
+        <a href="/login">Login</a> 
+        <span> | </span> 
+        <a href="/register"><button class="btn btn-sm btn-outline-secondary">Register</button></a>
+    </div>
+  </nav>
+</div>
